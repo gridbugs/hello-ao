1769a2fb596e2076604c62af91de55c5f5f22d4b
Stephen Sherratt <stephen@sherra.tt>
2025-03-28 16:03:19 +1100

    Oscillographics demo

--- main.ml
+++ main.ml
@@ -52,6 +52,35 @@ module Sync_channel = struct
   ;;
 end
 
+module Viz_queue = struct
+  module Writer = struct
+    type 'a t = { queue : 'a Queue.t }
+
+    let push t x = Queue.push x t.queue
+  end
+
+  type 'a t =
+    { queue : 'a Queue.t
+    ; mutex : Mutex.t
+    }
+
+  let create () = { queue = Queue.create (); mutex = Mutex.create () }
+
+  let drain t ~f =
+    Mutex.protect t.mutex (fun () ->
+      let rec loop () =
+        match Queue.take_opt t.queue with
+        | Some x ->
+          f x;
+          loop ()
+        | None -> ()
+      in
+      loop ())
+  ;;
+
+  let with_writer t ~f = Mutex.protect t.mutex (fun () -> f { Writer.queue = t.queue })
+end
+
 module Signal = struct
   module Ctx = struct
     type t = { sample_rate_hz : float }
@@ -60,6 +89,11 @@ module Signal = struct
   type t = Ctx.t -> float
 
   let sample (t : t) ctx = t ctx
+
+  module Arith = struct
+    let ( + ) a b ctx = a ctx +. b ctx
+    let ( * ) a b ctx = a ctx *. b ctx
+  end
 end
 
 module Stereo_signal = struct
@@ -104,6 +138,8 @@ module Player = struct
     { device; byte_format }
   ;;
 
+  let close t = Ao.close t.device
+
   let sample_to_byte_pair_lo_hi sample =
     (* Clamp the sample between -1 and 1. *)
     let sample_clamped = Float.min sample 1.0 |> Float.max (-1.0) in
@@ -133,6 +169,7 @@ module Player = struct
     assert (channels == 2);
     let ready_to_swap = Sync_channel.create () in
     let ready_to_gen = Sync_channel.create () in
+    let viz_queue = Viz_queue.create () in
     let buffer_size_in_bytes = bits / 8 * channels * buffer_size_in_samples in
     let buffer = Double_buffer.create buffer_size_in_bytes in
     let ctx = { Signal.Ctx.sample_rate_hz = float_of_int rate } in
@@ -146,18 +183,20 @@ module Player = struct
     let _generator =
       Domain.spawn (fun _ ->
         while true do
-          for i = 0 to buffer_size_in_samples - 1 do
-            let left_sample, right_sample = Stereo_signal.sample stereo_signal ctx in
-            let left_lo, left_hi = sample_to_byte_pair_lo_hi left_sample in
-            let right_lo, right_hi = sample_to_byte_pair_lo_hi right_sample in
-            write_bytes_to_buffer
-              buffer.generate_into
-              (i * 4)
-              ~left_lo
-              ~left_hi
-              ~right_lo
-              ~right_hi
-          done;
+          Viz_queue.with_writer viz_queue ~f:(fun writer ->
+            for i = 0 to buffer_size_in_samples - 1 do
+              let left_sample, right_sample = Stereo_signal.sample stereo_signal ctx in
+              Viz_queue.Writer.push writer (left_sample, right_sample);
+              let left_lo, left_hi = sample_to_byte_pair_lo_hi left_sample in
+              let right_lo, right_hi = sample_to_byte_pair_lo_hi right_sample in
+              write_bytes_to_buffer
+                buffer.generate_into
+                (i * 4)
+                ~left_lo
+                ~left_hi
+                ~right_lo
+                ~right_hi
+            done);
           Sync_channel.Send.send ready_to_swap.send;
           Sync_channel.Recv.recv ready_to_gen.recv
         done)
@@ -171,14 +210,62 @@ module Player = struct
           Sync_channel.Send.send ready_to_gen.send
         done)
     in
-    ()
+    viz_queue
+  ;;
+end
+
+module Renderer = struct
+  open Tsdl
+
+  type t = { renderer : Sdl.renderer }
+
+  let width = 640
+  let height = 480
+
+  let create () =
+    (match Sdl.init Sdl.Init.video with
+     | Error (`Msg e) -> failwith (Printf.sprintf "Init error: %s" e)
+     | Ok () -> ());
+    let _window, renderer =
+      match Sdl.create_window_and_renderer ~w:width ~h:height Sdl.Window.windowed with
+      | Error (`Msg e) -> failwith (Printf.sprintf "Create window error: %s" e)
+      | Ok x -> x
+    in
+    { renderer }
+  ;;
+
+  let rec is_window_closed () =
+    let event = Sdl.Event.create () in
+    if Sdl.poll_event (Some event)
+    then (
+      let typ = Sdl.Event.get event Sdl.Event.typ in
+      if typ == Sdl.Event.quit
+      then (
+        Sdl.quit ();
+        true)
+      else is_window_closed ())
+    else false
+  ;;
+
+  let render_points t ~f =
+    Sdl.set_render_draw_color t.renderer 0 0 0 255 |> Result.get_ok;
+    Sdl.render_clear t.renderer |> Result.get_ok;
+    Sdl.set_render_draw_color t.renderer 0 255 0 255 |> Result.get_ok;
+    let width = float_of_int width in
+    let height = float_of_int height in
+    f ~add_sample:(fun (x, y) ->
+      let x = int_of_float ((x *. width /. 2.) +. (width /. 2.)) in
+      let y = int_of_float ((y *. height /. 2.) +. (height /. 2.)) in
+      let rect = Sdl.Rect.create ~w:4 ~h:4 ~x ~y in
+      Sdl.render_fill_rect t.renderer (Some rect) |> Result.get_ok);
+    Sdl.render_present t.renderer
   ;;
 end
 
 let const x (_ctx : Signal.Ctx.t) = x
 
-let oscillator_sine freq_hz =
-  let state_01 = ref 0.0 in
+let oscillator_sine ?(offset_01 = 0.0) freq_hz =
+  let state_01 = ref offset_01 in
   fun (ctx : Signal.Ctx.t) ->
     let freq_hz = Signal.sample freq_hz ctx in
     let delta = freq_hz /. ctx.sample_rate_hz in
@@ -186,18 +273,40 @@ let oscillator_sine freq_hz =
     sin (!state_01 *. Float.pi *. 2.0)
 ;;
 
-let () =
-  let player = Player.create () in
-  let stereo_signal =
-    Stereo_signal.make
-      ~left:(oscillator_sine (const 200.))
-      ~right:(oscillator_sine (const 300.))
+let make_signal () =
+  let freq = 150.0 in
+  let left =
+    let open Signal.Arith in
+    let lfo =
+      ((oscillator_sine (const (freq *. 0.49)) + const 1.0) * const 0.25) + const 0.25
+    in
+    oscillator_sine (const freq + (oscillator_sine (const 0.01) * const 0.5)) * lfo
   in
-  Player.run_async player ~buffer_size_in_samples:256 stereo_signal;
+  let right =
+    let open Signal.Arith in
+    let lfo =
+      ((oscillator_sine ~offset_01:0.25 (const (freq *. 1.51)) + const 1.0) * const 0.25)
+      + const 0.25
+    in
+    oscillator_sine (const (freq *. 1.5)) * lfo
+  in
+  Stereo_signal.make ~left ~right
+;;
+
+let () =
+  let stereo_signal = make_signal () in
+  let player = Player.create () in
+  let viz_queue = Player.run_async player ~buffer_size_in_samples:256 stereo_signal in
+  let frame_delay = 1.0 /. 60. in
+  let renderer = Renderer.create () in
   let rec loop () =
-    print_endline "hello";
-    Unix.sleepf 1.0;
-    loop ()
+    if Renderer.is_window_closed ()
+    then Player.close player
+    else (
+      Renderer.render_points renderer ~f:(fun ~add_sample ->
+        Viz_queue.drain viz_queue ~f:(fun sample -> add_sample sample));
+      Unix.sleepf frame_delay;
+      loop ())
   in
   loop ()
 ;;
